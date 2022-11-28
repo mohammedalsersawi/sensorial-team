7c083d85e1727a3c3fa09f554f1411403f0c7881
mohammed alsersawi <107371667+mohammedalsersawi@users.noreply.github.com>
2022-11-28 22:26:30 +0200

    first commit

---
+++ resources/views/instructor/master.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <x-instructorPart.header />
+
+
+</head>
+
+<body data-topbar="colored">
+
+    <!-- <body data-layout="horizontal" data-topbar="colored"> -->
+
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+
+
+            <x-instructorPart.navbar />
+
+
+
+        <!-- ========== Left Sidebar Start ========== -->
+        <div class="vertical-menu">
+            <x-instructorPart.sidebar />
+
+        </div>
+        <!-- Left Sidebar End -->
+
+
+
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+
+            <div class="page-content">
+                @yield('content')
+                <!-- Container-Fluid -->
+            </div>
+            <!-- End Page-content -->
+
+
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-12 text-center">
+                            <script>
+                                document.write(new Date().getFullYear())
+                            </script> © Agroxa <span class="d-none d-sm-inline-block">- Crafted with <i
+                                    class="mdi mdi-heart text-primary"></i> by
+                                Hamo.</span>
+                        </div>
+
+                    </div>
+                </div>
+            </footer>
+        </div>
+        <!-- end main content-->
+
+    </div>
+    <!-- END layout-wrapper -->
+
+
+
+    <!-- JAVASCRIPT -->
+    <script src="adminassets/libs/jquery/jquery.min.js"></script>
+    <script src="adminassets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="adminassets/libs/metismenu/metisMenu.min.js"></script>
+    <script src="adminassets/libs/simplebar/simplebar.min.js"></script>
+    <script src="adminassets/libs/node-waves/waves.min.js"></script>
+    <script src="adminassets/libs/jquery-sparkline/jquery.sparkline.min.js"></script>
+
+    <!-- Responsive Table js -->
+    <script src="adminassets/libs/admin-resources/rwd-table/rwd-table.min.js"></script>
+
+    <!-- Init js -->
+    <script src="adminassets/js/pages/table-responsive.init.js"></script>
+
+    <!-- App js -->
+    <script src="adminassets/js/app.js"></script>
+
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('users.welcome');
+});
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('admin.empty');
+    });
+});
+
+
+Route::prefix('instructor')->group(function () {
+    Route::get('/', function () {
+        return view('instructor.empty');
+    });
+});
